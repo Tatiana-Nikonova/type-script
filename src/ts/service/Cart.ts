15e7215126c26dd36f45d529cd8b1cc0892ea1f1
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-01-09 23:41:34 +0300

    Cart.ts исправлено

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,29 +1,26 @@
 import Buyable from '../domain/Buyable';
 
 export default class Cart {
-  private _items: Buyable[] = [];
+	private _items: Buyable[] = [];
 
-  add(item: Buyable): void {
-    this._items.push(item);
-  }
+	add(item: Buyable): void {
+		this._items.push(item);
+	}
 
-  get items(): Buyable[] {
-    return [...this._items];
-  }
+	get items(): Buyable[] {
+		return [...this._items];
+	}
 
-  getAllPrice(): number {
-    return this._items.reduce((a, b) => a + b.price, 0);
-  }
+	getAllPrice(): number {
+		return this._items.reduce((a: number, b: Buyable) => a + b.price, 0);
+	}
 
-  getAllPriceWithDiscont(discont: number): number {
-    return (1 - discont / 100) * this._items.reduce((a, b) => a + b.price, 0);
-  }
+	getAllPriceWithDiscont(discont: number): number {
+		return (1 - discont / 100) * this._items.reduce((a: number, b: Buyable) => a + b.price, 0);
+	}
 
-  delete(id: number): boolean {
-    const size = this._items.length;
-    const index = this._items.findIndex(item => item.id === id);
-    if (index < 0) return false;
-    this._items.splice(index, 1);
-    return size - 1 === this._items.length;
-  }
+	delete(id: number): Buyable[] {
+		const items: Buyable[] = this.items;
+		return items.filter((item: Buyable) => item.id !== id);
+	}
 }
